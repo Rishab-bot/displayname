8d12654560ed62a4a81016b52ab5d5a6b69d03d4
unknown <prataplavi@gmail.com>
2025-01-09 20:32:37 +0530

    changes pushed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { DisplayName } from './display.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayName />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/display.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+export function DisplayName(){
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [fullName, setFullName] = useState("");
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        setFullName(`${firstName} ${lastName}`)
+    } 
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h1>Full Name Display</h1>
+                <label htmlFor="firstName">First Name</label>
+                <input 
+                type="text" 
+                name="first" 
+                id="firstName"
+                onChange={(e)=>setFirstName(e.target.value)}
+                />
+                <br/>
+                <label htmlFor="secondName">Last Name</label>
+                <input 
+                type="text" 
+                name="second" 
+                id="secondName"
+                onChange={(e)=>setLastName(e.target.value)}
+                />
+                <br/>
+                <button>Submit</button>
+            </form>
+
+            <h4>Full Name : {fullName}</h4>
+        </div>
+    );
+}
